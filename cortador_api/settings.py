23877afde9d3f7a6c402c52cab28b833d2f9ffce
Guilherme Baldissera <guilhermebaldissera@gmail.com>
2021-10-21 00:51:56 -0300

    Adjust allowed hosts

--- cortador_api/settings.py
+++ cortador_api/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://cortador-api.herokuapp.com/', 'http://localhost:8000', 'http://127.0.0.1:8000']
+ALLOWED_HOSTS = ['https://cortador-api.herokuapp.com/', 'http://localhost:8000', 'http://127.0.0.1:8000', 'localhost']
 
 
 # Application definition
