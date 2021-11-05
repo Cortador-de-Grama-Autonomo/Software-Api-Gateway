8b84bb731713739e4786af84953dd6ff94358f18
Guilherme Baldissera <guilhermebaldissera@gmail.com>
2021-11-04 21:53:32 -0300

    Adicionando novas propriedades no cortador de crama

--- base/models.py
+++ base/models.py
@@ -4,10 +4,18 @@ from django.db.models.fields.reverse_related import OneToOneRel
 
 # Create your models here.
 class GrassMachine(models.Model):
+    name = models.CharField(max_length=25, default='')
+    serie_number = models.CharField(max_length=25, null=True)
+    battery_percentage = models.IntegerField(null=True)
     model = models.CharField(max_length=10, null=True)
+    power = models.IntegerField(null=True)
+    voltage = models.IntegerField(null=True)
+    motor_type = models.CharField(max_length=20, null=True)
+    cut_type = models.CharField(max_length=20, null=True)
+    rotation_number = models.IntegerField(null=True)
     qr_code_identifier = models.CharField(max_length=20, unique=True, default='')
-    description = models.TextField(null=True)
     owner = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
 
     def __str__(self) -> str:
         return self.model + self.qr_code_identifier
